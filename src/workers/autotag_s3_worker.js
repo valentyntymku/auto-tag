c4d9368ef8291ec54c7e002f62d1d04eb91dcf7c
Valentyn Tymku <vtymk@softserveinc.com>
2020-07-28 17:30:03 +0300

    upd s3

--- src/workers/autotag_s3_worker.js
+++ src/workers/autotag_s3_worker.js
@@ -1,24 +1,42 @@
-import AWS from 'aws-sdk';
-import AutotagDefaultWorker, { AUTOTAG_TAG_NAME_PREFIX } from './autotag_default_worker';
+"use strict";
 
-class AutotagS3Worker extends AutotagDefaultWorker {
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _awsSdk = _interopRequireDefault(require("aws-sdk"));
+
+var _autotag_default_worker = _interopRequireWildcard(require("./autotag_default_worker"));
+
+function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return null; var cache = new WeakMap(); _getRequireWildcardCache = function () { return cache; }; return cache; }
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || typeof obj !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+class AutotagS3Worker extends _autotag_default_worker.default {
   /* tagResource
   ** method: tagResource
   **
   ** Tag the S3 bucket with the relevant information
   */
-
+ 
   async tagResource() {
     const roleName = this.roleName;
     const credentials = await this.assumeRole(roleName);
-    this.s3 = new AWS.S3({
+    this.s3 = new _awsSdk.default.S3({
       region: this.event.awsRegion,
       credentials
     });
-    let tags = await this.getExistingTags();
-    // remove anything starting with the prefix before we add our tags to make this idempotent
-    tags = tags.filter(tag => (!tag.Key.startsWith(AUTOTAG_TAG_NAME_PREFIX)));
-    tags = tags.concat(await this.getAutotagTags());
+    
+    let existingTags = await this.getExistingTags(); 
+    let autoTags = await this.getAutotagTags();
+    
+    autoTags = autoTags.filter(item => !existingTags.find(exItem => exItem.Key == item.Key));
+
+    let tags = existingTags.concat(autoTags);
+
     await this.setTags(tags);
   }
 
@@ -26,7 +44,7 @@ class AutotagS3Worker extends AutotagDefaultWorker {
     return new Promise((resolve, reject) => {
       try {
         this.s3.getBucketTagging({
-          Bucket: this.getBucketName(),
+          Bucket: this.getBucketName()
         }, (err, res) => {
           if (err) {
             if (err.code === 'NoSuchTagSet' && err.statusCode === 404) {
@@ -70,6 +88,8 @@ class AutotagS3Worker extends AutotagDefaultWorker {
   getBucketName() {
     return this.event.requestParameters.bucketName;
   }
+
 }
 
-export default AutotagS3Worker;
+var _default = AutotagS3Worker;
+exports.default = _default;
